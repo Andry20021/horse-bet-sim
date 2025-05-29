8de5e6ab76d110f6b69e228486c2305b23ca861e
Andry20021 <106783235+Andry20021@users.noreply.github.com>
2025-05-29 17:02:23 -0500

    small fixes
    Removed unused imports.
    Added firebase hosting cache to .gitignore.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,9 +24,6 @@ import {
   increment,
   collection,
   addDoc,
-  getDocs,
-  query,
-  where,
 } from 'firebase/firestore';
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
